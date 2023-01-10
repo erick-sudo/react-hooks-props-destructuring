4f213701140db5bc0a014aff430f13a958bce30a
Erick Ochieng <erickochieng766@gmail.com>
2023-01-10 13:31:50 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MovieCard from "./MovieCard";
 
 function App() {
-  const title = "Mad Max";
+  const title = "MAD MAX";
   const posterURL =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
